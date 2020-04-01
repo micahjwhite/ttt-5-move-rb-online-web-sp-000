654f6f58e059bd714a1437675de9a96f02986c0e
Micah White <micahjwhite@gmail.com>
2020-04-01 15:03:39 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,6 +10,9 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 def move(board, index, character = "X")
   board[index] = character
 end
+
