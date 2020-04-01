e2bcec27d068ac7949797620ea05c88721d2b94c
Micah White <micahjwhite@gmail.com>
2020-04-01 11:16:55 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -14,7 +14,7 @@ def move(board, index, character = "X")
   def update_array_at_with(board, index, character)
     board[index] = character
   end
-  update_array_at_with(board, 0, "X")
+  update_array_at_with(board, index, character)
 end
 
 end 
