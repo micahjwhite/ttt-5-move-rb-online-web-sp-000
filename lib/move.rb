582b4c2c0fc2440330a311a6a91f97768215d83a
Micah White <micahjwhite@gmail.com>
2020-04-01 01:54:46 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,6 +7,6 @@ def display_board(board)
 end
 
 user_input = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-def input_to_index(input)
-  puts "#{input["
+def input_to_index(user_input)
+  puts "#{input[] - 1}"
 end
