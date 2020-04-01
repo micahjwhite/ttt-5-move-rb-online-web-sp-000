0cebf69566413f85893b3bd57971723c342e968e
Micah White <micahjwhite@gmail.com>
2020-04-01 11:16:21 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,11 +11,10 @@ def input_to_index(user_input)
 end
 
 def move(board, index, character = "X")
-  return move(board, 0, character)
-end
-
-def update_array_at_with(board, index, character)
-  board[index] = character
+  def update_array_at_with(board, index, character)
+    board[index] = character
+  end
+  update_array_at_with(board, index, character)
 end
 
 end 
